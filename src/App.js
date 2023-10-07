f2c48cfd0a125142579a1749fa1e0abef913293b
Saurabh-Pandey79 <81317644+Saurabh-Pandey79@users.noreply.github.com>
2023-10-08 03:41:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import MinutesHours from './components/MinutesHours';
+import EpochConvy from './components/EpochConvy'; 
+
+function App() {
+  return (
+    <div className='container'>
+      <MinutesHours/>
+      <EpochConvy/>
+    </div>
+  );
+}
+
+export default App;
